563c77eddc50a156b01cb6e5e8238a6865f42563
maha035 <mahasohail466@gmail.com>
2026-06-18 18:31:17 -0800

    add grid box layout

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+let num = 16;
+if (num % 4 == 0){
+    document.write(true);
+}
+if(num % 2 == 0){
+    document.write(true);
+}else{
+    document.write(false);
+}
+if(!(num % 2 == 0)){
+    document.write(true);
+}else{
+    document.write(false);
+}
