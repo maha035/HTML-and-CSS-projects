2f506efabef3055d7958147af0fbdb9d35e5e393
maha035 <mahasohail466@gmail.com>
2026-06-20 18:00:50 -0800

    add afde in smooth transition tooltip

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,2 @@
-let num = 16;
-if (num % 4 == 0){
-    document.write(true);
-}
-if(num % 2 == 0){
-    document.write(true);
-}else{
-    document.write(false);
-}
-if(!(num % 2 == 0)){
-    document.write(true);
-}else{
-    document.write(false);
-}
+let hour = 24;
+let message = (hour > 12 ? 'good morning')
